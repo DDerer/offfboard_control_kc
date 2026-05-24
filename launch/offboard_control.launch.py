2bd6469424045f709a315b1c6f8754376836cebb
DDerer <3542518451@qq.com>
2026-05-24 19:35:41 +0800

    Delete some params

--- launch/offboard_control.launch.py
+++ launch/offboard_control.launch.py
@@ -9,12 +9,6 @@ def generate_launch_description():
         [
             DeclareLaunchArgument("auto_start", default_value="false"),
             DeclareLaunchArgument("auto_arm", default_value="false"),
-            DeclareLaunchArgument("warmup_setpoints", default_value="10"),
-            DeclareLaunchArgument("position_tolerance", default_value="0.1"),
-            DeclareLaunchArgument(
-                "local_position_topic",
-                default_value="/fmu/out/vehicle_local_position_v1",
-            ),
             Node(
                 package="offboard_control_cpp",
                 executable="offboard_node",
@@ -24,9 +18,6 @@ def generate_launch_description():
                     {
                         "auto_start": LaunchConfiguration("auto_start"),
                         "auto_arm": LaunchConfiguration("auto_arm"),
-                        "warmup_setpoints": LaunchConfiguration("warmup_setpoints"),
-                        "position_tolerance": LaunchConfiguration("position_tolerance"),
-                        "local_position_topic": LaunchConfiguration("local_position_topic"),
                     }
                 ],
             ),
